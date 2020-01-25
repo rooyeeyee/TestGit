0b9c89e86a779b31067672bcb971b085f811eeb1
eclipse_name <eclipse_user@qq.com>
2020-01-25 17:49:35 +0800

    create a new branch hot_fix

--- src/main/java/com/qn/Happy.java
+++ src/main/java/com/qn/Happy.java
@@ -4,5 +4,6 @@ public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Lefting ....");
 		System.out.println("Right ....");
+		System.out.println("hot_fix....");
 	}
 }
